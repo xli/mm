b323493d7ad24be932e980b2db2f944b23a15223
xli <swing1979@gmail.com>
2008-08-05 21:13:27 +0800

    changed license to mit-license; move selecting_list out of processor

--- lib/mm/console/processor.rb
+++ lib/mm/console/processor.rb
@@ -1,53 +1,5 @@
-# Copyright (c) 2008 Li Xiao
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 module MM
   module Console
-    class SelectingList
-      def initialize(list, handle_item_class)
-        @list = list
-        @handle_item_class = handle_item_class
-      end
-      
-      def select_by_index(runtime, index)
-        if @list[index]
-          @handle_item_class.new(@list[index]).execute(runtime)
-        else
-          raise "Did I surppose you to select #{index}?"
-        end
-      end
-      
-      def ==(obj)
-        return false unless obj
-        obj.to_s == to_s && obj.instance_variable_get(:@handle_item_class) == @handle_item_class
-      end
-      
-      def to_s
-        if @list.blank?
-          '! > Nothing.'
-        else
-          ret = []
-          @list.each_with_index do |obj, index|
-            ret << "#{index}) #{obj}"
-          end
-          ret << ''
-          ret << "! > Type index number to select item from list.\n "
-          ret.join("\n")
-        end
-      end
-    end
-    
     class Processor
       class EmptyContext
         def push(tokens)
